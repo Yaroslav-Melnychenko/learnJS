dcaf6b58eff2adbdc11f1354d14750e73b78a0ab
Yaroslav Melnychenko <yaroslav@scanovate.com>
2020-10-20 22:41:12 +0300

    lesson 2

--- script/index.js
+++ script/index.js
@@ -128,15 +128,114 @@
 // const a = [[12, 34], [1234, 56]];
 // console.log(a);
 
-setInterval(function() {
-    // Do
-    const r = Math.round(Math.random() * 255);
-    const g = Math.round(Math.random() * 255);
-    const b = Math.round(Math.random() * 255);
-    
-    console.log(r, g, b);
-    
-    const colored = document.getElementById('colored');
-    colored.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-}, 1000);
+// setInterval(function() {
+//     // Do
+//     const r = Math.round(Math.random() * 255);
+//     const g = Math.round(Math.random() * 255);
+//     const b = Math.round(Math.random() * 255);
 
+//     const width = Math.round(Math.random() * 300) + 100;
+//     const height = Math.round(Math.random() * 300) + 100;
+    
+//     console.log(width);
+    
+//     const colored = document.getElementById('colored');
+//     colored.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+//     colored.style.width = width + 'px';
+//     colored.style.height = height + 'px';
+// }, 1000);
+
+
+// const arr = ['Vasya', 'Kolya', 'Vitya', 'Andrey'];
+
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(a[i]);
+// }
+
+// arr.forEach()
+
+// function hello(name) {
+//     console.log(`hello ${name}`);
+// };
+
+// for(let i = 0; i < arr.length; i++) {
+//     hello(arr[i]);
+// }
+
+// const users = ['Admin', 'Agent', 'Manager'];
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// const c = add(10, 20);
+// // console.log('c', c);
+
+// const getFirstArrayItem = function(arr) {
+//     return arr[0];
+// }
+
+// const first = getFirstArrayItem([122, 2, 3])
+
+// // console.log('first', first);
+
+// const getRandomNumber = () => {
+//     const arr = [5, 25, 125];
+
+//     const index = Math.round(Math.random() * 2);
+
+//     return arr[index];
+// }
+
+// console.log('getRandomNumber', getRandomNumber());
+
+// console.log(hello());
+
+// const hello = function() {
+//     console.log('hello');
+// }
+
+// const fruits = ['Apple', 'Orange', 'Banana', 'Tangarine'];
+
+// fruits.forEach(function(item, i, arr) {
+//     console.log(`fruit is ${item}`);
+// });
+
+// const numbers = [1, 3, -2, 4, -24, 23, -33, -11];
+
+// const newArray = [];
+// for(let i = 0; i < numbers.length; i++) {
+//     if(numbers[i] > 0) newArray.push(numbers[i])
+// };
+
+// console.log(newArray);
+
+// const newArray = numbers.filter(function(item) {
+//     return item < 0
+// })
+
+// console.log('newArray', newArray);
+
+// const aa = [1, 5, 6, 7, 8];
+
+// const newArray = aa.map(function(item) {
+//     return item * 2;
+// });
+
+// console.log('newArray', newArray);
+
+// const aa = [1, 5, 6, 7, 8];
+
+// const is = aa.some(function(item) {
+//     return item % 2 === 0
+// });
+
+// console.log('is', is);
+
+const arr = [1, 2, 3, 4, 5];
+
+const result = arr.reduce(function(sum, current) {
+    return sum + current;
+});
+
+console.log('result', result);
